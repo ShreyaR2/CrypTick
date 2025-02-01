ff534b86bd659baecf155546bafd5fec6d29d42c
NaikVedhas <vedhasnaik121@gmail.com>
2025-02-01 14:54:17 +0530

    Routing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
 import { createBrowserRouter,createRoutesFromElements,RouterProvider,Route } from "react-router-dom"
 
 import Layout from "./Layout"
+import LandingPage from "./pages/LandingPage"
+import Home from "./pages/Home"
 
 const App = () => {
   
   
     const router = createBrowserRouter(
         createRoutesFromElements(
-            <Route to={<Layout/>}>
-
+            <Route  path={'/'} element={<Layout/>}>
+                <Route path="" element={<LandingPage/>}/>
+                <Route path="home" element={<Home/>}/>
             </Route>
         )
     )
-    
 
     return (
-    <div>
-
-    </div>
+    <>
+        <RouterProvider  router={router}/>
+    </>
   )
 }
 export default App
--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Routes, Link, Outlet } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
-// Layout Component
+import {Outlet} from "react-router-dom"
+
 const Layout = () => {
+
+
   return (
     <div className="flex flex-col min-h-screen">
       <ScrollToTop/>
